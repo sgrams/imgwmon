d15c81a7e027c66185f8926a6e4f53fc57683688
sgrams <sjg@fmdx.pl>
2017-01-09 12:56:30 +0100

    JSON parser added

--- imgwmon.c
+++ imgwmon.c
@@ -6,9 +6,32 @@
 
 void	printInfo	(void);
 char	*getData	(int station_id);
+struct MemoryStruct
+{
+	char *memory;
+	size_t size;
+};
 
 static int	station_id = 253190220; /*  default ID number */
 
+/* funkcja zapisująca do char* dla curla  */
+static size_t
+WriteMemoryCallback(void *contents, size_t size, size_t nmemb, void *userp)
+{
+	size_t realsize = size * nmemb;
+	struct MemoryStruct *mem = (struct MemoryStruct *)userp;
+
+	mem->memory = realloc(mem->memory, mem->size + realsize + 1);
+	if (mem->memory == NULL)
+	{
+		printf("Not enough memory!\n");
+		return 0;
+	}
+	memcpy(&(mem->memory[mem->size]), contents, realsize);
+	mem->size += realsize;
+	mem->memory[mem->size] = 0;
+	return realsize;
+}
 
 int main (int argc, char **argv)
 {
@@ -22,9 +45,10 @@ int main (int argc, char **argv)
 		{"id",			required_argument,	0,	'i'},
 	};
 	
+	char *dane;
 	int option_index = 0;
 	int c;
-	
+	dane = malloc(1);
 	if(argc!=1)
 	{
 		while (1)
@@ -61,20 +85,25 @@ int main (int argc, char **argv)
 					abort();
 			}
 		}
-		getData(station_id);
+		dane = getData(station_id);
+		free(dane);
 	}
 	else
 		printInfo();
-	
+
 	return EXIT_SUCCESS;
 }
 
-char *getData (int station_id, MemoryStruct chunk)
+char *getData (int station_id)
 { 
 	CURL *curl_handle;
 	CURLcode res;
 	char url[] = "http://monitor.pogodynka.pl/api/station/meteo/?id=";
 	char buffer[10];
+	struct MemoryStruct chunk;
+
+	chunk.memory = malloc(1);
+	chunk.size = 0;
 
 	snprintf(buffer, sizeof(buffer), "%d", station_id);
 	strcat(url, buffer);
@@ -83,10 +112,9 @@ char *getData (int station_id, MemoryStruct chunk)
 	curl_handle = curl_easy_init();
 
 	curl_easy_setopt(curl_handle, CURLOPT_URL, url);
+	curl_easy_setopt(curl_handle, CURLOPT_USERAGENT, "imgwmon-agent/0.1");
 	curl_easy_setopt(curl_handle, CURLOPT_WRITEFUNCTION, WriteMemoryCallback);
 	curl_easy_setopt(curl_handle, CURLOPT_WRITEDATA, (void *)&chunk);
-	curl_easy_setopt(curl_handle, CURLOPT_USERAGENT, "imgwmon-agent/0.1");
-	
 	res = curl_easy_perform(curl_handle);
 	if (res != CURLE_OK)
 	{
@@ -94,7 +122,8 @@ char *getData (int station_id, MemoryStruct chunk)
 	}
 	curl_easy_cleanup(curl_handle);
 	curl_global_cleanup();
-	return chunk;
+	
+	return chunk.memory;
 }
 
 void printInfo (void)
