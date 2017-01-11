50d58e9c23974deb89bbfface66d97187fbccfce
sgrams <sjg@fmdx.pl>
2017-01-11 14:35:02 +0100

    Fixed a lot of things

--- imgwmon.c
+++ imgwmon.c
@@ -4,11 +4,12 @@
 #include <getopt.h>
 #include <time.h>
 #include <curl/curl.h>
-#include <yajl/yajl_parse.h>
+#include <errno.h>
+#include "jsmn.h"
 
-static int station_id = 253190220;
 void printInfo (void);
-char *getData (int station_id);
+char *getData (int target_id);
+
 static size_t
 WriteMemoryCallback (void *contents, size_t size, size_t nmemb, void *userp);
 
@@ -20,7 +21,6 @@ struct MemoryStruct
 
 int main (int argc, char **argv)
 {
-	static int verbose_flag = 0;
 	static struct option long_options[] =
 	{
 		{"help", no_argument, 0, 'h'},
@@ -30,7 +30,8 @@ int main (int argc, char **argv)
 		{"id", required_argument, 0, 'i'},
 		{0, 0, 0, 0},
 	};
-	char *types_of_data[] =
+
+	static char *types_of_data[] =
 	{
 		"hourlyPrecipRecords",
 		"dailyPrecipRecords",
@@ -44,13 +45,23 @@ int main (int argc, char **argv)
 		"windMaxVelocityRecords",
 		NULL
 	};
-	char *data;
-	int option_index = 0;
-	int c, i = 0;
 
-	data = malloc(1);
-
-	if(argc>1)
+	char *target_type_of_data = "temperatureAutoRecords";
+	char target_time[21];
+	char *temp_target_type_of_data;
+	char *data	= malloc(1);
+	
+	int target_id			= 253190220;
+	int verbose_flag	= 0;
+	int option_index	= 0;
+	int c, i;
+	
+	time_t temp_time = time(NULL);
+	struct tm *real_time_struct;
+	
+	real_time_struct = gmtime(&temp_time);
+	strftime(target_time, 21, "%Y-%m-%dT%H:00:00Z", real_time_struct);
+	if (argc > 1)
 	{
 		while ((c = getopt_long (argc, argv, "hvd:t:i:", long_options, &option_index)) != -1)
 		{
@@ -61,32 +72,78 @@ int main (int argc, char **argv)
 					break;
 				case 'h':
 					printInfo();
-					exit(0);
+					return EXIT_SUCCESS;
 				case 'd':
-					if (optarg == NULL || strlen(optarg)!=17)
+					if ((strcmp(target_type_of_data, "dailyPrecipRecords") == 0 && strlen(optarg) != 10) ||
+						(strcmp(target_type_of_data, "tenMinutesPrecipRecords") == 0 && strlen(optarg) != 5))
+					{
 						if (verbose_flag)
-							fprintf(stderr, "The specified date is invalid, using latest UTC full-hour.\n");
+							fprintf(stderr, "The specified date is invalid, using latest possible time (%s).\n", target_time);
+						printf("target_time=%s\n", target_time);
+					}
+					else if (strcmp(target_type_of_data, "dailyPrecipRecords") == 0 && strlen(optarg) == 10)
+					{
+						/* YYYY-MM-DD specified */
+						strcpy(target_time, optarg);
+						strncat(target_time, "T06:00:00Z", 7);
+					}
+					else if (strcmp(target_type_of_data, "tenMinutesPrecipRecords") == 0 && strlen(optarg) == 5)
+					{
+						/* HH:MM specified */
+						strftime(target_time, 12, "%Y-%m-%dT", real_time_struct);
+						strncat(target_time, optarg, 6);
+						strncat(target_time, ":00Z", 1);
+					}
+					else if (strlen(optarg)!=16)
+					{
+						/* Invalid date */
+						if (verbose_flag)
+							fprintf(stderr, "The specified date is invalid, using latest possible time (%s).\n", target_time);
+					}
+					else
+					{
+						/* Correct date of common types, YYYY-MM-DD HH:MM (minutes=00)*/
+						strcpy(target_time, optarg);
+						strcat(target_time, ":00Z");
+						target_time[10] = 'T';
+					}
 					break;
 				case 't':
-					strcat(optarg, "Records");
+					temp_target_type_of_data = malloc(strlen(optarg)+strlen("Records")+1);
+					strncpy(temp_target_type_of_data, optarg, strlen(optarg)+1);
+					strcat(temp_target_type_of_data, "Records");
+
 					for (i=0; *(types_of_data+i) != NULL; i++)
-						if (strcmp(optarg, *(types_of_data+i)) == 0)
+						if (strcmp(temp_target_type_of_data, *(types_of_data+i)) == 0)
 							break;
+					
 					if (*(types_of_data+i) == NULL)
 					{
 						if (verbose_flag)
-							fprintf(stderr, "The specified type of data is invalid.\n");
-						return EXIT_FAILURE;
+							fprintf(stderr, "The specified type of data is invalid, using defaults.\n");
+					}
+					else
+					{
+						target_type_of_data = malloc(strlen(temp_target_type_of_data) + 1);
+						strcpy(target_type_of_data, temp_target_type_of_data+'\0');
+						if (strcmp(target_type_of_data, "dailyPrecipRecords") == 0 ||
+								strcmp(target_type_of_data, "windDirectionTelRecords") == 0 ||
+								strcmp(target_type_of_data, "windVelocityTelRecords") == 0 ||
+								strcmp(target_type_of_data, "windMaxVelocityRecords") == 0)
+						{
+							real_time_struct->tm_min = real_time_struct->tm_min - (real_time_struct->tm_min)%10;
+							strftime(target_time, 21, "%Y-%m-%dT%H:%M:00Z", real_time_struct);
+						}
 					}
 					break;
 				case 'i':
 					if (optarg == NULL || atoi(optarg)==0 || strlen(optarg) != 6)
 					{
-						if(verbose_flag)
+						if (verbose_flag)
 							fprintf(stderr, "The specified ID is invalid, using defaults.\n");
 					}
 					else
-						station_id = atoi(optarg);
+						target_id = atoi(optarg);
 					break;
 				case '?':
 					return EXIT_FAILURE;
@@ -94,18 +151,16 @@ int main (int argc, char **argv)
 					return EXIT_SUCCESS;
 			}
 		}
-		data = getData(station_id);
-		if(verbose_flag)
-			printf("%s\n", data);
+		data = getData(target_id);
+		printf("target_time=%s\n", target_time);
 	}
 	else
 		printInfo();
-
 	free(data);
 	return EXIT_SUCCESS;
 }
 
-char *getData (int station_id)
+char *getData (int target_id)
 { 
 	CURL *curl_handle;
 	CURLcode res;
@@ -116,7 +171,7 @@ char *getData (int station_id)
 	chunk.memory = malloc(1);
 	chunk.size = 0;
 
-	snprintf(buffer, sizeof(buffer), "%d", station_id);
+	snprintf(buffer, sizeof(buffer), "%d", target_id);
 	strcat(url, buffer);
 
 	curl_global_init(CURL_GLOBAL_ALL);
@@ -147,6 +202,7 @@ void printInfo (void)
       "\t-d <date>\tSet the date of fetching data (date format=\"YYYY-MM-DD HH:MM\", if empty - fetching latest\n"
       "\t-t <type>\tSet the type of fetching data (default=\"temperatureAuto\")\n\n"
 			"\tList of available data types:\n"
+			"\tcurrentPrecip\t\t - state of the precipitation at the moment\n"
 			"\thourlyPrecip\t\t - record of a precipitation per hour\n"
 			"\tdailyPrecip\t\t - record of a precipitation per day (date format=\"YYYY-MM-DD\")\n"
 			"\ttenMinutesPrecip\t - record of a precipitation per 10 minutes (date format=\"HH:MM\")\n"
@@ -160,6 +216,7 @@ void printInfo (void)
 			"\tThe data is available up to the last three days.\n"
 			);
 }
+
 static size_t
 WriteMemoryCallback(void *contents, size_t size, size_t nmemb, void *userp)
 {
